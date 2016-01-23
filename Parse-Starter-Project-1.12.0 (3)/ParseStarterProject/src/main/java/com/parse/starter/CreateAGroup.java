e63c383b1ccb94c828d276b559217bcb5cfb2ab2
Jaimie <jaimiec@seas.upenn.edu>
2016-01-23 11:52:08 -0500

    Stuff
    things

---
+++ Parse-Starter-Project-1.12.0 (3)/ParseStarterProject/src/main/java/com/parse/starter/CreateAGroup.java
@@ -0,0 +1,9 @@
+package com.parse.starter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jaimie on 1/23/2016.
+ */
+public class CreateAGroup extends AppCompatActivity {
+}
--- Parse-Starter-Project-1.12.0 (3)/ParseStarterProject/src/main/java/com/parse/starter/MaintainGroups.java
+++ Parse-Starter-Project-1.12.0 (3)/ParseStarterProject/src/main/java/com/parse/starter/MaintainGroups.java
@@ -2,9 +2,12 @@ package com.parse.starter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.LogInCallback;
@@ -12,16 +15,37 @@ import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 /**
  * Created by Jaimie on 1/23/2016.
  */
-public class MaintainGroups {
+public class MaintainGroups extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+       ParseUser p = ParseUser.getCurrentUser();
+        p.get
+       final ListView listView = (ListView) findViewById(R.id.listView2);
+       ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
 
     }
 
@@ -47,7 +71,10 @@ public class MaintainGroups {
         return super.onOptionsItemSelected(item);
     }
 
-
+    public void createGroup (View view) {
+        Intent intent =  new Intent(this, CreateAGroup.class);
+        startActivity(intent);
+    }
 
 
 }
